cdd9314e45eb6dec9b8ec87cd20ab710295982cf
PowerEpicDephys <100228943+PowerEpicDephys@users.noreply.github.com>
2022-10-13 16:11:05 -0400

    updating gains

--- controllers.py
+++ controllers.py
@@ -85,7 +85,7 @@ class SawickiWickiController(Controller):
                 self._update_setpoint(theta0=self.ankle_angles[1])
 
         if self.is_taught and self.found_setpt:
-            self.exo.update_gains(Kp=20, Ki=200, Kd=0, ff=60)  #Mod and test AMRO
+            self.exo.update_gains(Kp=20, Ki=150, Kd=0, ff=60)  #Mod and test AMRO
             # super().command_gains()
             # print('engaged..., desired k_val: ', self.k_val,
             #       'setpoint: ', self.ankle_angles[0])
